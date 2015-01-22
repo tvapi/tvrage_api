a34db19f3ca4818082a438c1190fc24c19de8621
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2015-01-22 08:33:33 +0100

    remove unused methods

--- lib/tvrage_api/base.rb
+++ lib/tvrage_api/base.rb
@@ -3,38 +3,6 @@ require 'service_api'
 class TvrageApi::Base
   include ServiceApi::BaseFaraday
 
-  def normalize_recaps_options(*options)
-    normalize_options({ id: :show }, *options)
-  end
-
-  def normalize_last_recaps_options(*options)
-    normalize_options([:days], *options)
-  end
-
-  def normalize_schedule_options(*options)
-    normalize_options([:country], *options)
-  end
-
-  def normalize_full_schedule_options(*options)
-    normalize_options([:country, '24_formats'.to_sym], *options)
-  end
-
-  def normalize_search_options(*options)
-    normalize_options({ name: :show }, *options)
-  end
-
-  def normalize_show_options(*options)
-    normalize_options({ id: :sid }, *options)
-  end
-
-  def normalize_show_with_episode_options(*options)
-    normalize_options({ show_id: :sid, episode: :ep }, *options)
-  end
-
-  def normalize_update_options(*options)
-    normalize_options([:hours, :sort, :since], *options)
-  end
-
   private
 
   def base_url
